a8c6c934ce060ba9962bf01919eca9d8983a6361
Larisa Stupina <ls@esurance.ch>
2024-01-05 14:55:58 +0100

    Code-cleaned aircompany project

--- JS/Planes/MilitaryPlane.js
+++ JS/Planes/MilitaryPlane.js
@@ -5,12 +5,10 @@ class MilitaryPlane extends Plane {
     constructor(model, maxSpeed, maxFlightDistance, maxLoadCapacity, militaryType) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.militaryType = militaryType;
-
     }
 
     getMilitaryType() {
         return this.militaryType;
     }
 }
-
 module.exports = MilitaryPlane;
--- JS/Planes/Plane.js
+++ JS/Planes/Plane.js
@@ -15,13 +15,13 @@ class Plane {
         return this.maxSpeed;
     }
 
-    Get_Max_Flight_Distance() {
+    getMaxFlightDistance() {
         return this.maxFlightDistance;
     }
 
     getMinLoadCapacity() {
-     let result = this.maxLoadCapacity;
-     return result;
+        let result = this.maxLoadCapacity;
+        return result;
     }
 }
 
