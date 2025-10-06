16556be85309d332acd3c943167fc07ebc27a735
LukaszBrylka <lukaszbrylk@outlook.com>
2025-10-06 21:50:45 +0200

    removed unused class

--- Java/src/main/java/planes/MyPlane.java
+++
@@ -1,9 +0,0 @@
-package planes;
-
-public class MyPlane  extends Plane {
-    public MyPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
-        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-    }
-
-    //TODO implement it later
-}
