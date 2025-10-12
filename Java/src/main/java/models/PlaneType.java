835ccfa634e04890167e3f5ee3b8d533557bc659
LukaszBrylka <lukaszbrylk@outlook.com>
2025-10-12 16:38:54 +0200

    enum formating changed

--- Java/src/main/java/models/PlaneType.java
+++ Java/src/main/java/models/PlaneType.java
@@ -2,10 +2,15 @@ package models;
 
 public class PlaneType {
     public enum Experimental {
-        LIFTING_BODY, HYPERSONIC, HIGH_ALTITUDE, VTOL
+        LIFTING_BODY,
+        HYPERSONIC,
+        HIGH_ALTITUDE,
+        VTOL
     }
 
     public enum Military {
-        FIGHTER, BOMBER, TRANSPORT
+        FIGHTER,
+        BOMBER,
+        TRANSPORT
     }
 }
