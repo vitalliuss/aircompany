1a469a0e73f6753cbaa23930349fa073ba17f1e1
Vitali Shulha <vitalliuss@gmail.com>
2025-11-26 18:46:51 +0400

    Refactor Java module clean code

---
+++ Java/src/main/java/Planes/ExperimentalPlane.java
@@ -0,0 +1,46 @@
+package Planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalTypes;
+
+public class ExperimentalPlane extends Plane {
+
+    private final ExperimentalTypes type;
+    private ClassificationLevel classificationLevel;
+
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.type = type;
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ExperimentalTypes getType() {
+        return type;
+    }
+
+    public ClassificationLevel getClassificationLevel() {
+        return classificationLevel;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel) {
+        this.classificationLevel = classificationLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ExperimentalPlane{" +
+                "type=" + type +
+                ", classificationLevel=" + classificationLevel +
+                "} " + super.toString();
+    }
+}
